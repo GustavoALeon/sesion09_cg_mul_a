ecc37c1e7df6564c3ec2b5b651d09eaee0468525
Gustavo Leon <est.gustavo.leon@unimilitar.edu.co>
2021-09-27 10:13:39 -0500

    Signed-off-by: Gustavo Leon <est.gustavo.leon@unimilitar.edu.co>

--- 01-04.js
+++ 01-04.js
@@ -23,7 +23,8 @@ function cubo(x, y, z, color, material, alambrado){
     
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
 
-    // add the cube to the scene
+    
+     // add the cube to the scene
     scene.add(cube);
     return(cube);
 }
@@ -43,17 +44,23 @@ function init() {
     scene.add(axes);
 
     Cubo = [];   // Definir un array unidimensional
-    Cubo.push(cubo(4, 4, 4, 0xFF0000, 'Physical', false));
-    Cubo.push(cubo(4, 4, 4, 0x00FFFF, 'Standard', false));
-    Cubo.push(cubo(4, 4, 4, 0xFFFFFF, 'Lambert', false));
-    Cubo.push(cubo(4, 4, 4, 0xFF00FF, 'Basic', false));
-    Cubo.push(cubo(4, 4, 4, 0xFF0000, 'Phong', false));
+    Cubo.push(cubo(4, 4, 4, 0xFFAAFF, 'Physical', false));
+    Cubo.push(cubo(4, 4, 4, 0xFFFFFF, 'Physical', false));
+    Cubo.push(cubo(4, 4, 4, 0xFFAACC, 'Physical', false));
+    Cubo.push(cubo(4, 4, 4, 0xAAAAFF, 'Physical', false));
+    Cubo.push(cubo(4, 4, 4, 0x00AAFF, 'Physical', false));
 
-    Cubo[0].position.set(0, 2, 0);
-    Cubo[1].position.set(0, 6, 0);
-    Cubo[2].position.set(0, 10, 0);
-    Cubo[3].position.set(0, 14, 0);
-    Cubo[4].position.set(0, 18, 0);
+    for(var x=0;x<5;x++)
+    {
+        var i=2+(4*x);
+        Cubo[x].position.set(0, i, 0)
+        
+    }
+    //Cubo[0].position.set(0, 2, 0);
+    //Cubo[1].position.set(0, 6, 0);
+    //Cubo[2].position.set(0, 10, 0);
+    //Cubo[3].position.set(0, 14, 0);
+    //Cubo[4].position.set(0, 18, 0);
 
     //Luz (requerida para el material MeshLambertMaterial)
     light = new THREE.PointLight(0xFFFF00); //  Luz proveniente de un punto en el espacio, 
