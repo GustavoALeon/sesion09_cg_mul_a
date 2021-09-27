efe288d37b99f61f32496c02a33783349a615666
Gustavo Leon <est.gustavo.leon@unimilitar.edu.co>
2021-09-27 09:25:48 -0500

    Signed-off-by: Gustavo Leon <est.gustavo.leon@unimilitar.edu.co>

--- 01-04.js
+++ 01-04.js
@@ -46,19 +46,22 @@ function init() {
     Cubo.push(cubo(4, 4, 4, 0xFF0000, 'Physical', false));
     Cubo.push(cubo(4, 4, 4, 0x00FFFF, 'Standard', false));
     Cubo.push(cubo(4, 4, 4, 0xFFFFFF, 'Lambert', false));
-    
+    Cubo.push(cubo(4, 4, 4, 0xFF00FF, 'Basic', false));
+    Cubo.push(cubo(4, 4, 4, 0xFF0000, 'Phong', false));
 
-    Cubo[0].position.set(-4, 9, 0);
-    Cubo[1].position.set(-4, 18, 0);
-    Cubo[2].position.set(-4, 27, 0);
+    Cubo[0].position.set(0, 2, 0);
+    Cubo[1].position.set(0, 6, 0);
+    Cubo[2].position.set(0, 10, 0);
+    Cubo[3].position.set(0, 14, 0);
+    Cubo[4].position.set(0, 18, 0);
 
     //Luz (requerida para el material MeshLambertMaterial)
     light = new THREE.PointLight(0xFFFF00); //  Luz proveniente de un punto en el espacio, 
-    light.position.set( -10, 40, 15 );             //  Localización de la luz. (x, y, z).
+    light.position.set( -10, 15, 15 );             //  Localización de la luz. (x, y, z).
     scene.add( light );
 
     // position and point the camera to the center of the scene
-    camera.position.set(-20, 60, 40);
+    camera.position.set(-35, 20, 45);
     camera.lookAt(scene.position);
 
     // add the output of the renderer to the html element
